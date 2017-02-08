1bf2b48ffe743b81d8317cbc09074355f624da20
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-07 19:17:06 -0500

    added shorcode field to AppURL model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class AppURL(models.Model):
     url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15, unique=True)
+    # shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
+    # shortcode = models.CharField(max_length=15, default='appdefaultshortcode')
 
     def __str__(self):
         return str(self.url)
